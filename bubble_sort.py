c67774f9cbb775bb7e67bccf04640586e9aac4f4
Akifum1 <a8115091@aoyama.jp>
2017-02-19 13:15:47 +0900

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,5 +6,5 @@ for i in range(len(num)-1):
         if num[j] > num[j+1]:
             tmp = num[j]
             num[j] = num[j+1]
-            num[j+1] = temp
+            num[j+1] = tmp
 print(num)
